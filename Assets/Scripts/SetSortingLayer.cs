e0c55e46bab1049ceb801daa1e8f510ad1ce869a
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-05-22 01:19:09 +0900

    パーティクル追加
    敵に当たったときのパーティクルの追加と、ソーティングレイヤー設定のためのスクリプト追加

---
+++ Assets/Scripts/SetSortingLayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetSortingLayer : MonoBehaviour {
+
+	//パーティクルのソーティングレイヤーがInspectorから変更できないようなので,
+	//ソーティングレイヤーを変更するだけのスクリプト（必要によっては他のスクリプトと合体させる）
+
+	public string layerName = "Smoke";
+
+	void Start () {
+	
+		GetComponent<Renderer>().sortingLayerName = layerName;
+
+	}
+	
+
+}
