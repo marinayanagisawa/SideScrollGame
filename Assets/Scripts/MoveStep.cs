7fcb9053edd2e7ced54418ca304d773388948a0f
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-05-27 00:33:41 +0900

    動く床に往復機能追加
    moveStepの横移動に、moveTimeで往復する機能を追加

--- Assets/Scripts/MoveStep.cs
+++ Assets/Scripts/MoveStep.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class MoveStep : MonoBehaviour {
 
 	public bool horizontal = true;
-	public float moveSpeed = 1f;
-	public float moveTime = 3f;
+	public float moveSpeed = 1.0f;
+	public float moveTime = 5.0f;
 	public GameObject step;
+	public bool moveR = true;
+	public bool moveL = false;
 
 
 	void Start () {
@@ -16,14 +18,39 @@ public class MoveStep : MonoBehaviour {
 	
 
 	void Update () {
-	//----------とりあえず移動まで,　横移動のStepに関しては,moveTime後に反対方向に移動するようにする
-		if (horizontal) {
-			step.GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, 0);
-		}
 
+		//横移動専用　moveTimeで反対に移動する
+		if (horizontal) {
+			if (moveR) {
+				Debug.Log ("moveRight");
+				StartCoroutine(MoveR ());
+
+			} else {
+				Debug.Log ("moveLeft");
+				StartCoroutine(MoveL());
+			}
+		}
 	
+
+		//こちらは縦移動専用
 		if (!horizontal){
 			step.GetComponent<Rigidbody2D> ().velocity= new Vector2 (0, -moveSpeed);
 		}
 	}
+
+
+	IEnumerator MoveR(){
+		step.GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, 0);
+		yield return new WaitForSeconds (moveTime);
+		moveR = false;
+		moveL = true;
+	}
+
+	IEnumerator MoveL(){
+		step.GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, 0);
+		yield return new WaitForSeconds (moveTime);
+		moveR = true;
+		moveL = false;
+	}
 }
+
