6f7f48dcf79ab291b3124484dfac464d67a056ff
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-05-30 00:19:26 +0900

    敵の接地判定と横移動を追加
    敵の接地判定と、横移動を実装。歩く敵と飛ぶ敵にクラスを分ける予定。

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float MoveSpeed = 5.0f;
+	//public GameObject enemy; 
+	public bool isGrounded;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		//GetComponent<Rigidbody2D> ().velocity = new Vector2 (-MoveSpeed, 0);
+		//レイヤーマスクの番号を指定
+		int layer = 1 << 9;
+		//接地判定
+		isGrounded = Physics2D.Linecast (transform.position, transform.position - transform.up * 1, layer);
+		}
+
+
+
+	/*
+	void FixedUpdate(){
+		//GetComponent<Rigidbody2D> ().AddForce ( new Vector2 (-MoveSpeed, 0));
+
+
+	}
+	*/
+
+}
---
+++ Assets/Scripts/WalkingEnemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkingEnemy : Enemy {
+
+	public float moveSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, 0);
+	}
+}
