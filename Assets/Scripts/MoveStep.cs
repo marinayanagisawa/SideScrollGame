cae011e6a97be20e2196dcbb306b3f8b8e3c5565
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-06-02 18:08:14 +0900

    移動する足場に上下移動を追加
    MoveStep.csに上下の処理を追加
    プレファブのStepAに左右、StepBに上下を設定

--- Assets/Scripts/MoveStep.cs
+++ Assets/Scripts/MoveStep.cs
@@ -7,36 +7,46 @@ public class MoveStep : MonoBehaviour {
 	public float moveSpeed = 1.0f;
 	public float moveTime = 5.0f;
 	public GameObject step;
-	public bool moveR = true;
-	public bool moveL = false;
-
+	private bool moveR = true;
+	private bool moveL = false;
+	private bool moveU = true;
+	private bool moveD = false;
 
 	void Start () {
-	
-		step = GameObject.Find("MoveStepA");
+
 	}
 	
 
 	void Update () {
 
-		//横移動専用　moveTimeで反対に移動する
+		//横移動専用　moveTimeで左右に移動する
 		if (horizontal) {
 			if (moveR) {
 				StartCoroutine(MoveR ());
 
-			} else {
+			} else if(moveL)  {
 				StartCoroutine(MoveL());
 			}
 		}
 	
 
-		//こちらは縦移動専用
+		//縦移動専用 moveTimeで上下に移動する
 		if (!horizontal){
-			step.GetComponent<Rigidbody2D> ().velocity= new Vector2 (0, -moveSpeed);
+			//step.GetComponent<Rigidbody2D> ().velocity= new Vector2 (0, -moveSpeed);
+
+
+			if (moveU) {
+				StartCoroutine(MoveU());
+
+			} else if(moveD) {
+				StartCoroutine(MoveD());
+			}
+
+
 		}
 	}
 
-
+	//左右の移動
 	IEnumerator MoveR(){
 		step.GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, 0);
 		yield return new WaitForSeconds (moveTime);
@@ -49,6 +59,21 @@ public class MoveStep : MonoBehaviour {
 		yield return new WaitForSeconds (moveTime);
 		moveR = true;
 		moveL = false;
+	}
+
+	//上下の移動
+	IEnumerator MoveU(){
+		step.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, moveSpeed);
+		yield return new WaitForSeconds (moveTime);
+		moveU = false;
+		moveD = true;
+	}
+
+	IEnumerator MoveD(){
+		step.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, -moveSpeed);
+		yield return new WaitForSeconds (moveTime);
+		moveU = true;
+		moveD = false;
 	}
 }
 
