f3a5256c6f8b305ae947ff19326e28433825c3b4
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-06-02 14:24:52 +0900

    敵の生成ポイントを作成
    プレイヤーとGeneratorが一定の距離になると、一度だけ（回数は暫定）敵を生成するプレファブを追加

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+
+	public GameObject robo;
+	public GameObject[] enemy;
+	public int enemyNum;
+	public float generateDis = 2.0f;
+
+	public float dis;
+
+
+	void Start () {
+		
+
+	}
+
+	void Update () {
+		
+		//ジェネレーターの位置を取得
+		Vector2 enemyPos = this.transform.position;
+		//プレイヤーの位置を取得
+		Vector2 roboPos = robo.transform.position;
+		//プレイヤーとジェネレーターの位置を比較
+		dis = Vector2.Distance (roboPos, enemyPos);
+		//Debug.Log (dis);
+
+		//敵を生成し,ジェネレーターを削除
+		if (dis < generateDis) {
+			Instantiate (enemy[enemyNum] , transform.position, transform.rotation);
+			Destroy (this);
+		}
+
+	}
+
+}
