0bf45f3e1fba365cbd7dacb1f5ebd24330d9ee21
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-06-10 14:37:19 +0900

    ステージギミックの作動スイッチを実装
    プレイヤーが近づくとギミックが作動し、作動後に削除される仕組みを追加（Gimmick.cs）
    不要なアニメーションの削除
    Scene1のステージ調整

---
+++ Assets/Scripts/Gimmick.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gimmick : MonoBehaviour {
+
+	//インスペクタから設定
+	public GameObject gimmickObj;
+	public Animator animator;
+
+	void Start () {
+		
+		animator = gimmickObj.GetComponent<Animator>();
+	}
+		
+	void OnTriggerEnter2D(Collider2D col){
+		string layerName = LayerMask.LayerToName (col.gameObject.layer);
+
+		//プレイヤーと当たったら,設定しておいたギミックが作動
+		if (layerName == "Chara") {
+			animator.SetTrigger("switch");
+
+			Destroy (gimmickObj, 2.0f);
+			Destroy (this);
+
+		}
+	}
+
+
+}
