c49cffbd4ca8ffd46ccfff03175f471d2fb6084d
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-06-21 14:54:14 +0900

    画面揺れを2段階に変更
    大きく揺らす場合のメソッドを追加

--- Assets/Scripts/Shaker.cs
+++ Assets/Scripts/Shaker.cs
@@ -3,35 +3,36 @@ using System.Collections;
 
 public class Shaker : MonoBehaviour {
 
-
+	//揺らしたいオブジェクトをインスペクターから設定
 	public GameObject camera;
 
+	//他のクラスからフラグを立てる
 	public bool shakeOn = false;
+	public bool shakeOnM = false;
 
-	// Use this for initialization
-	void Start () {
-	
-		//Shack (camera);
-
-	}
-	
-	// Update is called once per frame
 	void Update () {
-
+		//フラグを見て小揺れ
 		if (shakeOn) {
 			Shake (camera);
 			shakeOn = false;
 		}
 
+		//フラグを見て中揺れ
+		if (shakeOnM) {
+			ShakeM (camera);
+			shakeOnM = false;
+		}
 	}
 
 
 
-
+	//小揺れ用
 	public void Shake(GameObject obj){
-
 		iTween.ShakePosition(obj, iTween.Hash("x",0.1f,"y",0.1f,"time",0.7f));
-
 	}
-
+		
+	//中揺れ用
+	public void ShakeM(GameObject obj){
+		iTween.ShakePosition(obj, iTween.Hash("x",0.3f,"y",0.3f,"time",0.8f));
+	}
 }
