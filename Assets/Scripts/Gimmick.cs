fd55d508da3172cfa4d1dbbf027ebaff5bb6d3d2
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-06-21 14:37:50 +0900

    画面揺れの実装
    画面揺れの演出をするShakerクラスを追加

--- Assets/Scripts/Gimmick.cs
+++ Assets/Scripts/Gimmick.cs
@@ -10,10 +10,14 @@ public class Gimmick : MonoBehaviour {
 	//サウンド
 	private AudioSource sound;
 
+	public GameObject shaker;
+
 	void Start () {
-		
+
+		shaker = GameObject.Find ("Shaker");
 		animator = gimmickObj.GetComponent<Animator>();
 		sound = GetComponent<AudioSource> ();
+
 	}
 		
 	void OnTriggerEnter2D(Collider2D col){
@@ -23,6 +27,8 @@ public class Gimmick : MonoBehaviour {
 		if (layerName == "Chara") {
 			animator.SetTrigger("switch");
 			sound.PlayOneShot (sound.clip, 1.0f);
+			shaker.GetComponent<Shaker> ().shakeOn = true;
+
 
 			Destroy (gimmickObj, 2.0f);
 			Destroy (this);
---
+++ Assets/Scripts/Shaker.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shaker : MonoBehaviour {
+
+
+	public GameObject camera;
+
+	public bool shakeOn = false;
+
+	// Use this for initialization
+	void Start () {
+	
+		//Shack (camera);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (shakeOn) {
+			Shake (camera);
+			shakeOn = false;
+		}
+
+	}
+
+
+
+
+	public void Shake(GameObject obj){
+
+		iTween.ShakePosition(obj, iTween.Hash("x",0.1f,"y",0.1f,"time",0.7f));
+
+	}
+
+}
