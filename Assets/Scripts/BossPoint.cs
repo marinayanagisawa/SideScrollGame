e88e354b15c900a16f297a04cb2586edaf6e345e
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-07-19 14:41:18 +0900

    ボス出現処理メソッドの一部実装
    メソッドをコルーチンに変更
    プレイヤーの入力受付切り替え、カメラチェンジ、ボスのGravityScale、出現ポイントオブジェクトの削除などを実装

--- Assets/Scripts/BossPoint.cs
+++ Assets/Scripts/BossPoint.cs
@@ -5,7 +5,7 @@ public class BossPoint : MonoBehaviour {
 
 	private GameObject mainCamera;
 	private GameObject subCamera;
-
+	private PlayerController pc;
 	private GameObject robo;
 	private GameObject boss;
 
@@ -16,6 +16,8 @@ public class BossPoint : MonoBehaviour {
 		boss = GameObject.Find ("Boss");
 		boss.GetComponent<Rigidbody2D> ().gravityScale = 0;
 
+		pc = robo.GetComponent<PlayerController> ();
+		
 		//カメラを取得し,サブカメラは不使用にしておく
 		mainCamera = GameObject.Find ("Main Camera");
 		subCamera = GameObject.Find ("SubCamera");
@@ -24,24 +26,39 @@ public class BossPoint : MonoBehaviour {
 	
 	}
 
-	void OnTriggerEnter2D(Collider col){
+
+
+	void OnTriggerEnter2D(Collider2D col){
 		//ボス出現演出呼び出し
+		StartCoroutine("BossComeUp");
 		//BossPointの削除
+		//Destroy(this.gameObject);
 
 	}
 
 
-	void BossComeUp(){
+	IEnumerator BossComeUp(){
 		//Todo----------------------ボス出現演出
 		//BGMをストップ（GameController）
 		//プレイヤーのCanMoveをオフ
+		pc.canMove = false;
+		yield return new WaitForSeconds (1.0f);
 		//プレイヤーの背後に壁を作成
 		//カメラチェンジ呼び出し（メインからサブへ）
+		ChangeCamera();
+		yield return new WaitForSeconds (1.0f);
 		//BossのGravityScaleを３に変更
+		boss.GetComponent<Rigidbody2D> ().gravityScale = 3;
 		//Bossの着地後に画面揺れ
 		//カメラチェンジ呼び出し（サブからメインへ）
+		ChangeCamera ();
+		yield return new WaitForSeconds (1.0f);
 		//プレイヤーのCanMoveをオン
+		pc.canMove = true;
+		yield return new WaitForSeconds (1.0f);
 		//bossBGMの再生（GameControllerに追加）
+		Destroy(this.gameObject);
+
 	}
 
 
