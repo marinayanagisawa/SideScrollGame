9d1a9ab2b0feb0cd9d4149da8d0cfebad2edc52d
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-07-22 12:17:18 +0900

    ボスアームにガードコライダーを追加
    ボスアームで弾をガードできるように変更
    Guardオブジェクトをアームの子要素にし、Guardクラスを追加

---
+++ Assets/Scripts/Guard.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Guard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		string layerName = LayerMask.LayerToName (col.gameObject.layer);
+
+		if (layerName == "shot") {
+			Destroy (col.gameObject);
+
+		}
+	}
+}
+	
