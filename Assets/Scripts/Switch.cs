3aee91c9115c75ca1e70d6c41d5aea8d40688e56
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-07-28 10:44:52 +0900

    Issue#7看板落下を修正
    アニメーションなしで看板落下を作成
    Switchクラスを作成（Gimmickクラスは必要なくなったので、旧看板オブジェクトを使わないことが決まったら削除してOK）

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+
+	//インスペクタから設定
+	public GameObject gimmickObj;
+
+	private AudioSource sound;
+
+	void Start () {
+
+		sound = GetComponent<AudioSource> ();
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		string layerName = LayerMask.LayerToName (col.gameObject.layer);
+
+		//プレイヤーと当たったら,設定しておいたギミックが作動
+		if (layerName == "Chara") {
+			gimmickObj.GetComponent<Rigidbody2D> ().gravityScale = 1;
+			sound.PlayOneShot (sound.clip, 1.0f);
+
+			Destroy (gimmickObj, 2.0f);
+		}
+	}
+}
