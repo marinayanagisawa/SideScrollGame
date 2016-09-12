2dfc5c484d8e42ed708522d77cd823db93309bb4
marinayanagisawa <marinayanagisawacb@gmail.com>
2016-09-12 14:45:06 +0900

    リザルトのスタートのタイミング調整
    クリアサウンドと被らないように調整

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -189,7 +189,7 @@ public class GameController : MonoBehaviour {
 	//リザルト処理
 	IEnumerator Result(){
 		//少し待ってからスタート
-		yield return new WaitForSeconds (2.0f);
+		yield return new WaitForSeconds (3.0f);
 		//背景を暗くする
 		animator.SetTrigger ("Back");
 		Debug.Log ("ResultStart!!");
